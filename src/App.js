849819b5eebcc583de41c588d28a73104cbd0a62
BEENA-PAL <129384052+BEENA-PAL@users.noreply.github.com>
2024-01-19 22:24:39 +0530

    added index
    use effect

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./components/Header";
+import UseEffect from "./components/UseEffect";
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <UseEffect />
+    </div>
+  );
+};
+
+export default App;
